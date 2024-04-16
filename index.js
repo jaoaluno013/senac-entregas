e84beebcc78f552cb31558d07408ad54d6c1cb28
jaoaluno013 <166620309+jaoaluno013@users.noreply.github.com>
2024-04-16 11:50:04 -0300

    index

---
+++ index.js
@@ -0,0 +1,21 @@
+let nome = prompt('qual seu nome?')
+let idade= prompt(' qual sua idade?') 
+
+//let name= cleiton
+//let idade= 39
+
+nome = prompt('qual seu nome?')
+idade = prompt('qual sua idade?')
+
+console.log('tipo de varíavel nome', typeof nome)
+console.log('tipo de varíavel idade',typeof idade)
+
+
+
+
+
+
+
+
+
+
